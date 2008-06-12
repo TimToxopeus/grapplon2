627f19d3b97ae2faaeef5838f93954660b2a19d0
tim.toxopeus@gmail.com <tim.toxopeus@gmail.com@d38044ed-e64e-0410-b607-c9252a9fa7d4>
2008-06-12 09:30:44 +0000

    ElectroPlanet overlay

--- trunk/Grapplon/ElectroPlanet.h
+++ trunk/Grapplon/ElectroPlanet.h
@@ -1,15 +1,18 @@
 #pragma once
 
 #include "Planet.h"
+#include "AnimatedTexture.h"
 
 class CElectroPlanet : public CPlanet
 {
 private:
 	float m_fRespawnTime;
+	CAnimatedTexture *m_pElectric;
 
 public:
 	CElectroPlanet(PlanetaryData &data);
 	virtual ~CElectroPlanet();
 	
 	virtual void Render();
+	virtual void Update( float fTime );
 };
